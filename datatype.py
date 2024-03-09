84301a2295a3e7375b494b010b0fa3a56d411888
Nash Lee <nashlee168@gmail.com>
2024-03-10 00:14:48 +0800

    Initial Commit

---
+++ datatype.py
@@ -0,0 +1,26 @@
+import os
+os.system('clear')
+
+######################
+# DATA TYPE
+# Strings
+# Numbers
+# Lists[]
+# Tuples()
+# Dictionaries{}
+# Bollean true or false
+#######################
+
+
+first_name = "nash"
+age = 40
+fav_pizza = {"John" : "Pepperoni",
+             "Bob" : "Mushroom",
+             "Mary" : "Chees"             
+             }
+
+
+print (fav_pizza["Bob"])
+name = True
+print(name)
+       
---
+++ hellow.py
@@ -0,0 +1,21 @@
+import os
+os.system('clear')
+
+######################
+# DATA TYPE
+# Strings
+# Numbers
+# Lists[]
+# Tuples()
+# Dictionaries
+# Bollean
+#######################
+
+first_name = "nash"
+age = 40
+name = ("John", "Bob", "Mary")
+
+
+print (name[0])
+print (name)
+       
